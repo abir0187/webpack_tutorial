60360b726897898a26e1327c959ed39682b1a32f
Rezwan Abir <abir0187@aol.com>
2022-03-26 18:06:07 -0400

    new

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,13 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
         ]
     }
 }
