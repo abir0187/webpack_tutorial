17918545ab649f895633f193070c76ebd2a71464
Rezwan Abir <abir0187@aol.com>
2022-03-26 17:57:42 -0400

    new

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
                 test: /\.txt/,
                 type: 'asset/source'
             },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            }
         ]
     }
 }
