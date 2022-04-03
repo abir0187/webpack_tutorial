880fd9ea408e566e59baaf15eb0f348eab762dab
Rezwan Abir <abir0187@aol.com>
2022-04-03 16:36:23 -0400

    new

--- README.md
+++
@@ -1,8 +0,0 @@
-# Webpack Tutorial
-##Build
-Run ``` npm run build ``` 
-
-##Questions
-1. Are css extracted to a separate file? No style tag in head.
-2. Are js minified to a single bundle.js? 
-3. Are you able to use new js code using babel? 
--- src/hello-world.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
-import Heading from "./components/heading/heading";
-
-const helloWorldButton = new HelloWorldButton();
-const heading = new Heading();
-
-heading.render('hello world');
-helloWorldButton.render();
